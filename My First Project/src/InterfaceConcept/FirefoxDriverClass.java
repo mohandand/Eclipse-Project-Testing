97f44640ac32eb7f888443c9ac2078fb3860b689
Dandigam <Dandigam@192.168.100.6>
2020-04-05 22:16:16 +0530

    Interface Concet

---
+++ My First Project/src/InterfaceConcept/FirefoxDriverClass.java
@@ -0,0 +1,22 @@
+package InterfaceConcept;
+
+public abstract class FirefoxDriverClass implements WebDriveInterface {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	@Override
+	public void click() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendClick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ My First Project/src/InterfaceConcept/WebDriveInterface.java
@@ -0,0 +1,10 @@
+package InterfaceConcept;
+
+public interface WebDriveInterface {
+
+	
+	public void click();
+	
+	public void sendClick();
+	
+}
